7d20aca5d9f748daeddcb322faecbcff98615eea
Kyle Husmann <kyle.husmann@gmail.com>
2020-08-09 12:50:45 -0400

    small fix

--- bug.f
+++ bug.f
@@ -1,7 +1,7 @@
          SUBROUTINE F1()
          DOUBLE PRECISION WORK(2**18)
 * Use a smaller size here and we don't get read errors
-         DOUBLE PRECISION WORK(2**17)
+*         DOUBLE PRECISION WORK(2**17)
          CALL F2(WORK)
          END
 
